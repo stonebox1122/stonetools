8b2302bcaffce45b1d33b8083bd7e01393412a57
stonebox1122 <287779438@qq.com>
2019-06-03 22:04:57 +0800

    commit

---
+++ src/main/java/com/stone/datastructure/ArrayUtils.java
@@ -0,0 +1,193 @@
+package com.stone.datastructure;
+
+import java.util.Arrays;
+
+/**
+ * @author stone
+ * @date 2019/6/3 18:48
+ * description
+ */
+public class ArrayUtils {
+
+    /**
+     * 数组长度
+     * @param arr 数组名称
+     * @return
+     */
+    public static int getLength(Object[] arr){
+        return arr.length;
+    }
+
+    /**
+     * 打印数组内容到控制台
+     * @param arr
+     */
+    public static void getAll(Object[] arr){
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * 获取指定下标的元素
+     * @param arr
+     * @param index
+     * @return
+     */
+    public static Object getWithIndex(Object[] arr, int index){
+        if (index > arr.length - 1 || index < 0) {
+            throw new RuntimeException("下标越界");
+        }
+        return arr[index];
+    }
+
+    /**
+     * 在数组末尾添加一个元素
+     * @param arr
+     * @param element
+     * @return
+     */
+    public static Object[] addToLast(Object[] arr, Object element){
+        Object[] newArr = new Object[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+        newArr[arr.length] = element;
+        return newArr;
+    }
+
+    /**
+     * 在数组指定位置插入元素
+     * @param arr
+     * @param element
+     * @param index
+     * @return
+     */
+    public static Object[] addWithIndex(Object[] arr, Object element, int index){
+        if (index > arr.length || index < 0) {
+            throw new RuntimeException("下标越界");
+        }
+        Object[] newArr = new Object[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            if (i < index){
+                newArr[i] = arr[i];
+            } else {
+                newArr[i + 1] = arr[i];
+            }
+        }
+        newArr[index] = element;
+        return newArr;
+    }
+
+    /**
+     * 删除数组最后一个元素
+     * @param arr
+     * @return
+     */
+    public static Object[] deleteLast(Object[] arr){
+        Object[] newArr = new Object[arr.length - 1];
+        for (int i = 0; i < arr.length - 1; i++) {
+            newArr[i] = arr[i];
+        }
+        return newArr;
+    }
+
+    /**
+     * 删除数组指定位置元素
+     * @param arr
+     * @param index
+     * @return
+     */
+    public static Object[] deleteWithIndex(Object[] arr, int index){
+        if (index > arr.length - 1 || index < 0) {
+            throw new RuntimeException("下标越界");
+        }
+        Object[] newArr = new Object[arr.length - 1];
+        for (int i = 0; i < newArr.length; i++) {
+            if (i < index){
+                newArr[i] = arr[i];
+            } else {
+                newArr[i] = arr[i + 1];
+            }
+        }
+        return newArr;
+    }
+
+    /**
+     * 线性查找指定元素
+     * @param arr
+     * @param element
+     * @return
+     */
+    public static int linearSearch(Object[] arr, Object element){
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (element.equals(arr[i])){
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    /**
+     * 整数类型数据排序
+     * @param arr
+     * @return
+     */
+    public static int[] sort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    /**
+     * 二分查找法
+     * @param arr
+     * @param element
+     * @return
+     */
+    public static int binarySearch(int[] arr, int element){
+        int[] sortArr = sort(arr);
+        int index = -1;
+        int head = 0;
+        int tail = arr.length - 1;
+        while (true) {
+            int mid = (head + tail) / 2;
+            if (head > tail){
+                break;
+            }
+            if (sortArr[mid] == element){
+                index = mid;
+                break;
+            }
+            if(sortArr[mid] > element){
+                tail = mid - 1;
+            }
+            if (sortArr[mid] < element){
+                head = mid + 1;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        Object[] arr = new Object[]{"aa","bb","cc","dd"};
+        ArrayUtils.getAll(ArrayUtils.addToLast(arr, "ee"));
+        ArrayUtils.getAll(ArrayUtils.addWithIndex(arr,"ee",2));
+        ArrayUtils.getAll(ArrayUtils.deleteLast(arr));
+        ArrayUtils.getAll(ArrayUtils.deleteWithIndex(arr,2));
+        System.out.println(ArrayUtils.getWithIndex(arr, 2));
+        System.out.println(ArrayUtils.linearSearch(arr, "dd"));
+        int[] arr2 = new int[]{9,8,7,6,5,4,3};
+        System.out.println(Arrays.toString(ArrayUtils.sort(arr2)));
+        System.out.println(ArrayUtils.binarySearch(arr2, 8));
+
+    }
+
+}
--- src/main/java/com/stone/tools/FileUtils.java
+++ src/main/java/com/stone/tools/FileUtils.java
@@ -1,6 +1,7 @@
 package com.stone.tools;
 
 import java.io.*;
+import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -224,8 +225,81 @@ public class FileUtils {
         return list;
     }
 
+    /**
+     * 移动文件到目录
+     * @param srcFilePath
+     * @param targetDir
+     */
+    public static void moveFileToDir(String srcFilePath, String targetDir){
+        String targetFileName = new File(srcFilePath).getName();
+        String targetFilePath = targetDir + File.separator + targetFileName;
+        try {
+            FileUtils.copyFile(srcFilePath,targetFilePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            FileUtils.deleteFile(srcFilePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 监控目录
+     * @param targetDir
+     * （1）当文件修改时，会被调用两次，即输出两个相同的修改。
+     * （2）不能对其子文件夹进行监控，只能提示目录被修改。
+     * （3）无法对文件类型进行过滤。
+     */
+    public static void monitorDir(String targetDir){
+        Path path = Paths.get(targetDir);
+        try (WatchService watchService = FileSystems.getDefault().newWatchService()) {
+            path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE,
+                    StandardWatchEventKinds.ENTRY_MODIFY,
+                    StandardWatchEventKinds.ENTRY_DELETE);
+            while (true) {
+                final WatchKey key = watchService.take();
+                for (WatchEvent<?> watchEvent : key.pollEvents()) {
+                    final WatchEvent.Kind<?> kind = watchEvent.kind();
+                    if (kind == StandardWatchEventKinds.OVERFLOW) {
+                        continue;
+                    }
+                    //创建事件
+                    if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
+                        System.out.println("[新建]");
+                    }
+                    //修改事件
+                    if (kind == StandardWatchEventKinds.ENTRY_MODIFY) {
+                        System.out.println("修改]");
+                    }
+                    //删除事件
+                    if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
+                        System.out.println("[删除]");
+                    }
+                    // get the filename for the event
+                    final WatchEvent<Path> watchEventPath = (WatchEvent<Path>) watchEvent;
+                    final Path filename = watchEventPath.context();
+                    // print it out
+                    System.out.println(kind + " -> " + filename);
+
+                }
+                boolean valid = key.reset();
+                if (!valid) {
+                    break;
+                }
+            }
+
+        } catch (IOException | InterruptedException ex) {
+            System.err.println(ex);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(FileUtils.getFiles("D:\\temp\\FileShare\\JUNO"));
+//        System.out.println(FileUtils.getFiles("D:\\temp\\report"));
+//        FileUtils.moveFileToDir("D:\\temp\\report\\SPC000EK-001-Data.xls","D:\\temp\\report\\success");
+//        System.out.println(FileUtils.getFiles("D:\\temp\\report"));
+        FileUtils.monitorDir("D:\\temp\\report");
     }
 
 }
